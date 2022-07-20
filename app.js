6ac61ca7dd0230a59f8d14742988eac1b81836a3
[dylan simerly] <[dylansimerly@yahoo.com]>
2022-07-20 15:47:30 -0400

    Server Connection

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.listen(3000, function() {
+  console.log("server started on port 3000");
+});
