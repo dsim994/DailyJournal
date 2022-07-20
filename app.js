0cd2c03ad8f8bb9bdb83ebdb2764b3fbec7d7bbb
[dylan simerly] <[dylansimerly@yahoo.com]>
2022-07-20 18:10:49 -0400

    Set Up Home

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+const homeStartingContent = ["Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
-  res.render("home");
+  res.render("home", {homeStartingContent: homeStartingContent});
 });
 
 app.listen(3000, function() {
